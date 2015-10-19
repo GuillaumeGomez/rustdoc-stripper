ac57ff69165760e51a335c760b43f24487dabfbe
Guillaume Gomez <guillaume1.gomez@gmail.com>
2015-10-19 16:47:56 +0200

    New way to parse

--- src/main.rs
+++ src/main.rs
@@ -17,40 +17,87 @@ use std::io::{BufReader, SeekFrom, BufRead, Write, Read, Seek};
 use std::fmt::{Display, Formatter, Error};
 use std::path::PathBuf;
 
-struct CommentEntry {
-    line: String,
+/*struct CommentEntry {
     file: String,
-    comment_lines: Vec<String>,
+    text: String,
 }
 
 impl CommentEntry {
-    pub fn new(file: &str) -> CommentEntry {
-        CommentEntry {
-            line: String::new(),
-            file: file.to_owned(),
-            comment_lines: Vec::new(),
-        }
+    fn new(file: &str, text: &str) -> CommentEntry {
+        
     }
 }
 
 impl Display for CommentEntry {
     fn fmt(&self, f: &mut Formatter) -> Result<(), Error> {
-        let mut out = format!("=!={}\n=|={}\n", self.file, self.line);
+        //let mut out = format!("=!={}\n=|={}\n", self.file, self.type_);
+        let mut out = String::new();
 
         //println!("{}: {} => {:?}", self.file, self.line, self.comment_lines);
-        for comment in &self.comment_lines {
+        /*for comment in &self.comment_lines {
             out.push_str(&comment);
             out.push_str("\n");
+        }*/
+        //writeln!(f, "{}", out)
+        writeln!(f, "=!={}\n{}", self.file, self.text)
+    }
+}*/
+
+enum EventType {
+    Comment(String),
+    Type(TypeStruct),
+    InScope,
+    OutScope,
+}
+
+struct TypeStruct {
+    ty: Type,
+    parent: Option<Box<TypeStruct>>,
+    name: String,
+}
+
+impl TypeStruct {
+    fn new(ty: Type, name: &str) -> TypeStruct {
+        TypeStruct {
+            ty: ty,
+            name: name.to_owned(),
+            parent: None,
         }
-        writeln!(f, "{}", out)
     }
 }
 
-fn loop_over_files(path: &str, comments: &mut Vec<CommentEntry>) {
+#[derive(Debug)]
+enum Type {
+    Struct,
+    Mod,
+    Enum,
+    Fn,
+    Const,
+    Static,
+    Type,
+    Variant,
+}
+
+impl Type {
+    fn from(s: &str) -> Type {
+        match s {
+            "struct" => Type::Struct,
+            "mod" => Type::Mod,
+            "enum" => Type::Enum,
+            "fn" => Type::Fn,
+            "const" => Type::Const,
+            "static" => Type::Static,
+            "type" => Type::Type,
+            _ => Type::Variant,
+        }
+    }
+}
+
+fn loop_over_files(path: &str/*, comments: &mut Vec<CommentEntry>*/) {
     match fs::read_dir(path) {
         Ok(it) => {
             for entry in it {
-                check_path_type(entry.unwrap().path().to_str().unwrap(), comments);
+                check_path_type(entry.unwrap().path().to_str().unwrap()/*, comments*/);
             }
         }
         Err(e) => {
@@ -59,62 +106,57 @@ fn loop_over_files(path: &str, comments: &mut Vec<CommentEntry>) {
     }
 }
 
-fn move_reader(it: &mut usize, lines: &[&str], file: &str,
-               comments: &mut Vec<CommentEntry>) {
-    let mut cm = CommentEntry::new(file);
-
-    cm.comment_lines.push(lines[*it].to_owned());
-    *it += 1;
-    while *it < lines.len() {
-        if lines[*it].trim_left().starts_with("///") {
-            cm.comment_lines.push(lines[*it].to_owned());
-        } else {
-            break;
-        }
+fn move_to(words: &[&str], it: &mut usize, limit: &str) {
+    while (*it + 1) < words.len() && words[*it + 1] != limit {
         *it += 1;
     }
-    cm.line = lines[*it].to_owned();
-    comments.push(cm);
-    //write!(comments, "{}", cm).unwrap();
-    //comments.flush();
 }
 
-fn strip_comments(path: &str, comments: &mut Vec<CommentEntry>) {
+fn strip_comments(path: &str/*, comments: &mut Vec<CommentEntry>*/) {
     match File::open(path) {
         Ok(mut f) => {
-            let mut out_lines = vec!();
-            let mut content = String::new();
-            f.read_to_string(&mut content).unwrap();
-            let lines : Vec<&str> = content.split('\n').collect();
+            let mut b_content = String::new();
+            f.read_to_string(&mut b_content).unwrap();
+            let content = b_content.replace("{", " { ")
+                                   .replace("}", " } ")
+                                   .replace("///", "/// ")
+                                   .replace("\n", " \n ")
+                                   .replace("(", " (");
+            let b_content : Vec<&str> = b_content.split('\n').collect();
+            let words : Vec<&str> = content.split(' ').filter(|s| s.len() > 0).collect();
             let mut it = 0;
+            let mut line = 0;
+            let mut event_list = vec!();
 
-            while it < lines.len() {
-                let worker = lines[it].trim_left().to_owned();
-
-                if worker.starts_with("///") {
-                    // "normal" doc comments
-                    move_reader(&mut it, &lines, path, comments);
-                } else if worker.starts_with("//!")/* || worker.starts_with("/*!*/")*/ {
-                    // module comments
-                    move_reader(&mut it, &lines, path, comments);
+            while it < words.len() {
+                match words[it] {
+                    "///" => {
+                        event_list.push(EventType::Comment(b_content[line].to_owned()));
+                        move_to(&words, &mut it, "\n");
+                    }
+                    "struct" | "mod" | "fn" | "enum" | "const" | "static" | "type" => {
+                        event_list.push(EventType::Type(TypeStruct::new(Type::from(words[it]), words[it + 1])));
+                        it += 1;
+                    }
+                    "{" => {
+                        event_list.push(EventType::InScope);
+                    }
+                    "}" => {
+                        event_list.push(EventType::OutScope);
+                    }
+                    "\n" => {
+                        line += 1;
+                    }
+                    _ => {}
                 }
-                out_lines.push(lines[it].to_owned());
                 it += 1;
             }
-            let mut pb = PathBuf::from(path);
-            println!("{:?}", pb);
-            let tmp = format!("_{}", pb.file_name().unwrap().to_str().unwrap());
-            println!("{:?}", tmp);
-            pb.set_file_name(&tmp);
-            println!("{:?}", pb);
-            match OpenOptions::new().write(true).create(true).truncate(true).open(&pb) {
-                Ok(mut f) => {
-                    for line in out_lines {
-                        writeln!(f, "{}", line).unwrap();
-                    }
-                }
-                Err(e) => {
-                    println!("Unable to open \"{}\": {}", pb.to_str().unwrap(), e);
+            for event in event_list {
+                match event {
+                    EventType::Comment(s) => { println!("{}", s); }
+                    EventType::Type(t) => { println!("{:?} {}", t.ty, t.name);}
+                    EventType::InScope => { println!("{{"); }
+                    EventType::OutScope => { println!("}}"); }
                 }
             }
         }
@@ -124,20 +166,20 @@ fn strip_comments(path: &str, comments: &mut Vec<CommentEntry>) {
     }
 }
 
-fn check_path_type(path: &str, comments: &mut Vec<CommentEntry>) {
+fn check_path_type(path: &str/*, comments: &mut Vec<CommentEntry>*/) {
     match fs::metadata(path) {
         Ok(m) => {
             if m.is_dir() {
                 if path == ".." || path == "." {
                     return;
                 }
-                loop_over_files(path, comments);
+                loop_over_files(path/*, comments*/);
             } else {
                 if path == "./comments.cmts" {
                     return;
                 }
                 println!("-> {}", path);
-                strip_comments(path, comments);
+                strip_comments(path/*, comments*/);
             }
         }
         Err(e) => {
@@ -149,13 +191,13 @@ fn check_path_type(path: &str, comments: &mut Vec<CommentEntry>) {
 fn main() {
     println!("Starting...");
     match fs::remove_file("comments.cmts") { _ => {} }
-    let mut comments = vec!();
-    loop_over_files(".", &mut comments);
+    //let mut comments = vec!();
+    loop_over_files("."/*, &mut comments*/);
     match OpenOptions::new().write(true).create(true).truncate(true).open("comments.cmts") {
         Ok(mut f) => {
-            for com_entry in comments {
+            /*for com_entry in comments {
                 write!(f, "{}", com_entry).unwrap();
-            }
+            }*/
             println!("Done !");
         }
         Err(e) => {
@@ -163,3 +205,7 @@ fn main() {
         }
     }
 }
+
+// au lieu de splitter le fichier via '\n', lis le sur "une seule ligne".
+// quand tu croises un "///", tu stockes ça comme un commentaire, un "mod", comme un module, etc...
+// il faudra compter les '{' / '}' pour voir si t'es toujours danns le truc courant ou non
