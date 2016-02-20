19bbe64089cc7a70a80b74a8872c4083f728a99a
Gleb Kozyrev <gleb@gkoz.com>
2016-02-21 00:15:50 +0200

    Add more test cases and use tempdir

--- tests/tests.rs
+++ tests/tests.rs
@@ -12,13 +12,13 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-use std::fs::{File, create_dir, remove_file};
-use std::io::{Read, Write};
-use std::path::Path;
-
 extern crate stripper_lib;
+extern crate tempdir;
 
-const TEST_FILE_DIR : &'static str = "tests/files";
+use std::fs::File;
+use std::io::prelude::*;
+use std::path::Path;
+use tempdir::TempDir;
 
 const BASIC : &'static str = r#"/// struct Foo comment
 struct Foo {
@@ -58,28 +58,220 @@ mod Bar {
 }
 "#;
 
-fn get_basic_cmt(file: &str) -> String {
+fn get_basic_md(file: &str) -> String {
     format!(r#"<!-- file {} -->
 <!-- struct Foo -->
  struct Foo comment
 <!-- struct Foo§variant A -->
  Foo comment
  fn some_func(a: u32,
-              b: u32) {}
+              b: u32) {{}}
 <!-- mod Bar§macro test!§struct SuperFoo -->
  struct inside macro
 <!-- mod Bar§macro test!§macro sub_test!§struct FooFoo -->
  and another one!
-"#, file, "{}")
+"#, file)
 }
 
-fn gen_file(filename: &str, content: &str) -> File {
-    let mut f = File::create(&format!("{}/{}", TEST_FILE_DIR, filename)).expect("gen_file");
+const BASIC2 : &'static str = r#"
+use Bin;
+use Box;
+use Buildable;
+use Container;
+use Widget;
+use Window;
+use ffi;
+use glib::object::Downcast;
+use glib::object::IsA;
+use glib::translate::*;
+
+glib_wrapper! {
+/// Dialog boxes are a convenient way to prompt the user for a small amount
+/// of input, e.g. to display a message, ask a question, or anything else
+/// that does not require extensive effort on the user’s part.
+/// 
+/// ```
+/// {
+///  dialog = gtk_dialog_new_with_buttons ("Message",
+///                                        parent,
+///                                        flags,
+///                                        _("_OK"),
+///                                        GTK_RESPONSE_NONE,
+///                                        NULL);
+/// }
+/// ```
+    pub struct Dialog(Object<ffi::GtkDialog>): Widget, Container, Bin, Window, Buildable;
+
+    match fn {
+        get_type => || ffi::gtk_dialog_get_type(),
+    }
+}
+
+impl Dialog {
+    /// Creates a new dialog box.
+    /// 
+    /// Widgets should not be packed into this `Window`
+    /// directly, but into the `vbox` and `action_area`, as described above.
+    ///
+    /// # Returns
+    ///
+    /// the new dialog as a `Widget`
+    pub fn new() -> Dialog {
+        assert_initialized_main_thread!();
+        unsafe {
+            Widget::from_glib_none(ffi::gtk_dialog_new()).downcast_unchecked()
+        }
+    }
+
+    //pub fn new_with_buttons<T: IsA<Window>>(title: Option<&str>, parent: Option<&T>, flags: DialogFlags, first_button_text: Option<&str>, : /*Unknown conversion*//*Unimplemented*/Fundamental: VarArgs) -> Dialog {
+    //    unsafe { TODO: call ffi::gtk_dialog_new_with_buttons() }
+    //}
+}
+
+/// Trait containing all `Dialog` methods.
+pub trait DialogExt {
+    /// Adds an activatable widget to the action area of a `Dialog`,
+    /// connecting a signal handler that will emit the `Dialog::response`
+    /// signal on the dialog when the widget is activated. The widget is
+    /// appended to the end of the dialog’s action area. If you want to add a
+    /// non-activatable widget, simply pack it into the `action_area` field
+    /// of the `Dialog` struct.
+    fn add_action_widget<T: IsA<Widget>>(&self, child: &T, response_id: i32);
+
+    /// Adds a button with the given text
+    fn add_button(&self, button_text: &str, response_id: i32) -> Widget;
+}
+"#;
+
+const BASIC2_STRIPPED : &'static str = r#"
+use Bin;
+use Box;
+use Buildable;
+use Container;
+use Widget;
+use Window;
+use ffi;
+use glib::object::Downcast;
+use glib::object::IsA;
+use glib::translate::*;
+
+glib_wrapper! {
+    pub struct Dialog(Object<ffi::GtkDialog>): Widget, Container, Bin, Window, Buildable;
+
+    match fn {
+        get_type => || ffi::gtk_dialog_get_type(),
+    }
+}
+
+impl Dialog {
+    pub fn new() -> Dialog {
+        assert_initialized_main_thread!();
+        unsafe {
+            Widget::from_glib_none(ffi::gtk_dialog_new()).downcast_unchecked()
+        }
+    }
+
+    //pub fn new_with_buttons<T: IsA<Window>>(title: Option<&str>, parent: Option<&T>, flags: DialogFlags, first_button_text: Option<&str>, : /*Unknown conversion*//*Unimplemented*/Fundamental: VarArgs) -> Dialog {
+    //    unsafe { TODO: call ffi::gtk_dialog_new_with_buttons() }
+    //}
+}
+
+pub trait DialogExt {
+    fn add_action_widget<T: IsA<Widget>>(&self, child: &T, response_id: i32);
+
+    fn add_button(&self, button_text: &str, response_id: i32) -> Widget;
+}
+"#;
+
+fn get_basic2_md(file: &str) -> String {
+    format!(r#"<!-- file {} -->
+<!-- struct Dialog -->
+ Dialog boxes are a convenient way to prompt the user for a small amount
+ of input, e.g. to display a message, ask a question, or anything else
+ that does not require extensive effort on the user’s part.
+ 
+ ```
+ {{
+  dialog = gtk_dialog_new_with_buttons ("Message",
+                                        parent,
+                                        flags,
+                                        _("_OK"),
+                                        GTK_RESPONSE_NONE,
+                                        NULL);
+ }}
+ ```
+<!-- impl Dialog§fn new -->
+ Creates a new dialog box.
+ 
+ Widgets should not be packed into this `Window`
+ directly, but into the `vbox` and `action_area`, as described above.
+
+ # Returns
+
+ the new dialog as a `Widget`
+<!-- trait DialogExt -->
+ Trait containing all `Dialog` methods.
+<!-- trait DialogExt§fn add_action_widget -->
+ Adds an activatable widget to the action area of a `Dialog`,
+ connecting a signal handler that will emit the `Dialog::response`
+ signal on the dialog when the widget is activated. The widget is
+ appended to the end of the dialog’s action area. If you want to add a
+ non-activatable widget, simply pack it into the `action_area` field
+ of the `Dialog` struct.
+<!-- trait DialogExt§fn add_button -->
+ Adds a button with the given text
+"#, file)
+}
+
+const BASIC2_MD: &'static str = r#"<!-- file * -->
+<!-- struct Dialog -->
+ Dialog boxes are a convenient way to prompt the user for a small amount
+ of input, e.g. to display a message, ask a question, or anything else
+ that does not require extensive effort on the user’s part.
+ 
+ ```
+ {
+  dialog = gtk_dialog_new_with_buttons ("Message",
+                                        parent,
+                                        flags,
+                                        _("_OK"),
+                                        GTK_RESPONSE_NONE,
+                                        NULL);
+ }
+ ```
+<!-- impl Dialog§fn new -->
+ Creates a new dialog box.
+ 
+ Widgets should not be packed into this `Window`
+ directly, but into the `vbox` and `action_area`, as described above.
+
+ # Returns
+
+ the new dialog as a `Widget`
+<!-- impl Dialog§fn new_with_buttons -->
+ Creates a new `Dialog` with title `title` (or `None` for the default
+ title; see `Window::set_title`) and transient parent `parent` (or
+ `None` for none; see `Window::set_transient_for`).
+<!-- trait DialogExt -->
+ Trait containing all `Dialog` methods.
+<!-- trait DialogExt§fn add_action_widget -->
+ Adds an activatable widget to the action area of a `Dialog`,
+ connecting a signal handler that will emit the `Dialog::response`
+ signal on the dialog when the widget is activated. The widget is
+ appended to the end of the dialog’s action area. If you want to add a
+ non-activatable widget, simply pack it into the `action_area` field
+ of the `Dialog` struct.
+<!-- trait DialogExt§fn add_button -->
+ Adds a button with the given text
+"#;
+
+fn gen_file(temp_dir: &TempDir, filename: &str, content: &str) -> File {
+    let mut f = File::create(temp_dir.path().join(filename)).expect("gen_file");
     write!(f, "{}", content).unwrap();
     f
 }
 
-fn compare_files(expected_content: &str, file: &str) {
+fn compare_files(expected_content: &str, file: &Path) {
     let mut f = File::open(file).expect("compare_files '{}'");
     let mut buf = String::new();
     f.read_to_string(&mut buf).unwrap();
@@ -92,42 +284,59 @@ fn compare_files(expected_content: &str, file: &str) {
 }
 
 #[allow(unused_must_use)]
-fn clean_test(files_to_remove: &[&str]) {
-    for file in files_to_remove {
-        remove_file(file);
-    }
-}
-
-#[allow(unused_must_use)]
 #[test]
 fn test_strip() {
     let test_file = "basic.rs";
-    let comment_file = "basic.cmts";
-    clean_test(&[test_file, comment_file]);
-    create_dir(TEST_FILE_DIR);
+    let comment_file = "basic.md";
+    let temp_dir = TempDir::new("").unwrap();
+    gen_file(&temp_dir, test_file, BASIC);
     {
-        gen_file(test_file, BASIC);
-        let mut f = gen_file(comment_file, "");
-        stripper_lib::strip_comments(Path::new(TEST_FILE_DIR), test_file, &mut f, false);
+        let mut f = gen_file(&temp_dir, comment_file, "");
+        stripper_lib::strip_comments(temp_dir.path(), test_file, &mut f, false);
     }
-    compare_files(&get_basic_cmt(test_file), &format!("{}/{}", TEST_FILE_DIR, comment_file));
-    compare_files(BASIC_STRIPPED, &format!("{}/{}", TEST_FILE_DIR, test_file));
+    compare_files(&get_basic_md(test_file), &temp_dir.path().join(comment_file));
+    compare_files(BASIC_STRIPPED, &temp_dir.path().join(test_file));
 }
 
 #[allow(unused_must_use)]
 #[test]
 fn test_regeneration() {
-    let test_file = "regen.rs";
-    let comment_file = "regen.cmts";
-    clean_test(&[test_file, comment_file]);
-    create_dir(TEST_FILE_DIR);
+    let test_file = "basic.rs";
+    let comment_file = "basic.md";
+    let temp_dir = TempDir::new("").unwrap();
+    gen_file(&temp_dir, test_file, BASIC_STRIPPED);
+    gen_file(&temp_dir, comment_file, &get_basic_md(test_file));
+    stripper_lib::regenerate_doc_comments(temp_dir.path().to_str().unwrap(), false,
+                                          &temp_dir.path().join(comment_file).to_str().unwrap(),
+                                          false);
+    compare_files(BASIC, &temp_dir.path().join(test_file));
+}
+
+#[allow(unused_must_use)]
+#[test]
+fn test2_strip() {
+    let test_file = "basic.rs";
+    let comment_file = "basic.md";
+    let temp_dir = TempDir::new("").unwrap();
+    gen_file(&temp_dir, test_file, BASIC2);
     {
-        gen_file(test_file, BASIC);
-        let mut f = gen_file(comment_file, "");
-        stripper_lib::strip_comments(Path::new(TEST_FILE_DIR), test_file, &mut f, false);
-        stripper_lib::regenerate_doc_comments(TEST_FILE_DIR, false,
-                                              &format!("{}/{}", TEST_FILE_DIR, comment_file),
-                                              false);
+        let mut f = gen_file(&temp_dir, comment_file, "");
+        stripper_lib::strip_comments(temp_dir.path(), test_file, &mut f, true);
     }
-    compare_files(BASIC, &format!("{}/{}", TEST_FILE_DIR, test_file));
+    compare_files(&get_basic2_md(test_file), &temp_dir.path().join(comment_file));
+    compare_files(BASIC2_STRIPPED, &temp_dir.path().join(test_file));
+}
+
+#[allow(unused_must_use)]
+#[test]
+fn test2_regeneration() {
+    let test_file = "basic.rs";
+    let comment_file = "basic.md";
+    let temp_dir = TempDir::new("").unwrap();
+    gen_file(&temp_dir, test_file, BASIC2_STRIPPED);
+    gen_file(&temp_dir, comment_file, BASIC2_MD);
+    stripper_lib::regenerate_doc_comments(temp_dir.path().to_str().unwrap(), false,
+                                          &temp_dir.path().join(comment_file).to_str().unwrap(),
+                                          true);
+    compare_files(BASIC2, &temp_dir.path().join(test_file));
 }
